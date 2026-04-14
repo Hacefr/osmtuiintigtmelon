ccb8bce75b9f1beb692b38a073644c70eb3a44c8
Hacesgd <162230713+Hacefr@users.noreply.github.com>
2026-04-14 18:11:24 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/osmtuiintigtmelon', // This MUST match your repository name exactly!
   images: {
     unoptimized: true,
   },
