35acd20e3c39cc78e90e4c130baf6153f5466863
Hacesgd <162230713+Hacefr@users.noreply.github.com>
2026-04-14 17:54:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 const withPwa = require("next-pwa")({
-	dest: "public",
-	register: true,
-	skipWaiting: true,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPwa({
-	swcMinify: false,
-	reactStrictMode: true,
-	images: {
-		unoptimized: true,
-	},
+  swcMinify: false,
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 });
 
 module.exports = nextConfig;
